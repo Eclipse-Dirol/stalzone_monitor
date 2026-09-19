03a9a7032551984d67bc591835c17f61eb6e5d10
Eclipse <eclipse.dirol.personal@gmail.com>
2026-09-20 01:24:52 +0500

    created scapi for request to stalzone

---
+++ backend/api/stalzone_api.py
@@ -0,0 +1,44 @@
+from scapi import AppClient, OAuthClient
+import time
+import logging
+import json
+
+class SCAPI:
+    def __init__(self):
+        self.client = None
+
+        self._is_token_ready = False
+
+    async def _authorize(self):
+        if not self._is_token_ready:
+            with open('config.json', "r", encoding="utf-8") as f:
+                config_data = json.load(f)
+
+            client_id = config_data["client"]["CLIENT_ID"]
+            client_secret = config_data["client"]["CLIENT_SECRET"]
+            app_token = config_data["client"]["APP_TOKEN"]
+
+            if app_token == "":
+                authclient = OAuthClient(client_id=client_id, client_secret=client_secret)
+                app_token_obj = await authclient.get_app_token()
+                token = app_token_obj.access_token
+
+                config_data["client"]["APP_TOKEN"] = token
+                with open('config.json', "w", encoding="utf-8") as f:
+                    json.dump(config_data, f, ensure_ascii=False, indent=4)
+
+                self.client = AppClient(client_id=client_id, client_secret=client_secret)
+            else:
+                self.client = AppClient(token=app_token)
+
+            self._is_token_ready = True
+
+    async def auction(self, item_id: str):
+        t2 = time.perf_counter()
+        await self._authorize()
+        t3 = time.perf_counter()
+        lots = await self.client.auction(item_id).lots(limit=1)
+        t4 = time.perf_counter()
+        print(f"[DEBUG] Запрос к аукциону: {t4 - t3:.3f} сек")
+
+        return lots
