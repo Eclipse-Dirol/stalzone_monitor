91d4c87260b7b2dd158c5da103ee7df3204df0b0
Eclipse <eclipse.dirol.personal@gmail.com>
2026-09-20 13:43:23 +0500

    added constant_loading for websocket

--- backend/api/stalzone_api.py
+++ backend/api/stalzone_api.py
@@ -8,8 +8,9 @@ CONFIG_PATH = Path(__file__).resolve().parent.parent / "config.json"
 class SCAPI():
     def __init__(self):
         self.client = None
-
         self._is_token_ready = False
+        self.first_load = True
+        self.active_lots = []
 
     async def start_desktop(self):
         with open(CONFIG_PATH, "r", encoding="utf-8") as f:
@@ -52,12 +53,29 @@ class SCAPI():
 
             self._is_token_ready = True
 
-    async def auction(self, item_id: str):
+    async def request_for_auc(self, item_id: str, limit: int = 40):
         t2 = time.perf_counter()
         await self._authorize()
         t3 = time.perf_counter()
-        lots = await self.client.auction(item_id).lots(limit=1)
+        lots = await self.client.auction(item_id).lots(limit=limit)
         t4 = time.perf_counter()
+        print(f"[DEBUG] Авторизация: {t3 - t2:.3f} сек")
         print(f"[DEBUG] Запрос к аукциону: {t4 - t3:.3f} сек")
 
         return lots
+
+    async def constant_loading(self, item_id: str):
+        if self.first_load:
+
+            self.active_lots = await self.request_for_auc(item_id)
+            self.first_load = False
+
+            return self.active_lots
+        else:
+            new_lots = await self.request_for_auc(item_id, limit=1)
+
+            if new_lots:
+                self.active_lots.insert(0, new_lots[0])
+                if len(self.active_lots) > 40:
+                    self.active_lots.pop()
+            return self.active_lots
