340e19ffda613c8ce185ab07fb268c74b67f64c3
Eclipse <eclipse.dirol.personal@gmail.com>
2026-09-20 14:07:34 +0500

    reworked stalzone_api -> _authorize

--- backend/api/stalzone_api.py
+++ backend/api/stalzone_api.py
@@ -7,7 +7,6 @@ from pathlib import Path
 CONFIG_PATH = Path(__file__).resolve().parent.parent / "config.json"
 class SCAPI():
     def __init__(self):
-        self.client = None
         self._is_token_ready = False
         self.first_load = True
         self.active_lots = []
@@ -34,24 +33,8 @@ class SCAPI():
             with open(CONFIG_PATH, "r", encoding="utf-8") as f:
                 config_data = json.load(f)
 
-            client_id = config_data["client"]["CLIENT_ID"]
-            client_secret = config_data["client"]["CLIENT_SECRET"]
             app_token = config_data["client"]["APP_TOKEN"]
-
-            if app_token == "":
-                authclient = OAuthClient(client_id=client_id, client_secret=client_secret)
-                app_token_obj = await authclient.get_app_token()
-                token = app_token_obj.access_token
-
-                config_data["client"]["APP_TOKEN"] = token
-                with open('config.json', "w", encoding="utf-8") as f:
-                    json.dump(config_data, f, ensure_ascii=False, indent=4)
-
-                self.client = AppClient(token=token)
-            else:
-                self.client = AppClient(token=app_token)
-
-            self._is_token_ready = True
+            self.client = AppClient(token=app_token)
 
     async def request_for_auc(self, item_id: str, limit: int = 40):
         t2 = time.perf_counter()
