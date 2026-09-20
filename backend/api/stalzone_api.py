e604fbd952938709570f07e9b0a7550d2d420e82
Eclipse <eclipse.dirol.personal@gmail.com>
2026-09-20 12:19:17 +0500

    added new def start_desktop

--- backend/api/stalzone_api.py
+++ backend/api/stalzone_api.py
@@ -5,11 +5,28 @@ import json
 from pathlib import Path
 
 CONFIG_PATH = Path(__file__).resolve().parent.parent / "config.json"
-class SCAPI:
+class SCAPI():
     def __init__(self):
         self.client = None
 
         self._is_token_ready = False
+
+    async def start_desktop(self):
+        with open(CONFIG_PATH, "r", encoding="utf-8") as f:
+            config_data = json.load(f)
+        client_id = config_data["client"]["CLIENT_ID"]
+        client_secret = config_data["client"]["CLIENT_SECRET"]
+        try:
+            authclient = OAuthClient(client_id=client_id, client_secret=client_secret)
+            app_token_obj = await authclient.get_app_token()
+            token = app_token_obj.access_token
+            config_data["client"]["APP_TOKEN"] = token
+            with open('config.json', "w", encoding="utf-8") as f:
+                json.dump(config_data, f, ensure_ascii=False, indent=4)
+            return True
+        except Exception as e:
+            print(f"Лог авторизции: {e}")
+            return False
 
     async def _authorize(self):
         if not self._is_token_ready:
@@ -29,7 +46,7 @@ class SCAPI:
                 with open('config.json', "w", encoding="utf-8") as f:
                     json.dump(config_data, f, ensure_ascii=False, indent=4)
 
-                self.client = AppClient(client_id=client_id, client_secret=client_secret)
+                self.client = AppClient(token=token)
             else:
                 self.client = AppClient(token=app_token)
 
