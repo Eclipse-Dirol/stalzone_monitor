fbec21060f7a7403fc72e92cb3ab347f202ef217
Eclipse <eclipse.dirol.personal@gmail.com>
2026-09-20 13:46:35 +0500

    added new def which recive from start desktop (client_id, client_secret) and reworked recive lots in websocket

--- backend/api/server.py
+++ backend/api/server.py
@@ -1,5 +1,6 @@
 import asyncio
 from fastapi import FastAPI, HTTPException, WebSocket, WebSocketDisconnect
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from api.stalzone_api import SCAPI
 import json
@@ -10,21 +11,35 @@ api = SCAPI()
 
 class FrontendData(BaseModel):
     item_id: str
+
+class ClientData(BaseModel):
     client_id: str
     client_secret: str
 
 CONFIG_PATH = Path(__file__).resolve().parent.parent / "config.json"
 
-@app.post("/api/view_item")
-async def search_auction(payload: FrontendData):
+@app.post("/api/start")
+async def recive_request(client_data: ClientData):
     with open(CONFIG_PATH, "r", encoding="utf-8") as f:
         config_data = json.load(f)
-    config_data["client"]["CLIENT_ID"] = payload.client_id
-    config_data["client"]["CLIENT_SECRET"] = payload.client_secret
+    config_data["client"]["CLIENT_ID"] = client_data.client_id
+    config_data["client"]["CLIENT_SECRET"] = client_data.client_secret
     with open(CONFIG_PATH, "w", encoding="utf-8") as f:
         json.dump(config_data, f, ensure_ascii=False, indent=4)
 
-    lots = await api.auction(item_id=payload.item_id)
+    success = await api.start_desktop()
+
+    if not success:
+        raise HTTPException(
+            status_code=400,
+            detail={"error": "invalid client data", "error_description": "Не удалось получить токен. Проверьте ключи."}
+        )
+
+    return {"status": "success"}
+
+@app.post("/api/view_item")
+async def search_auction(payload: FrontendData):
+    lots = await api.request_for_auc(item_id=payload.item_id)
 
     if not lots:
         raise HTTPException(status_code=404, detail="Лоты не найдены")
@@ -42,10 +57,11 @@ async def auction_stream(websocket: WebSocket):
         while True:
             t0 = asyncio.get_running_loop().time()
 
-            lots = await api.auction(item_id=item_id)
+            lots = await api.constant_loading(item_id)
 
             if lots:
-                await websocket.send_json({"status": "update", "data": lots})
+                safe_lots = jsonable_encoder(lots)
+                await websocket.send_json({"status": "update", "data": safe_lots})
             else:
                 await websocket.send_json({"status": "error", "message": "Нет лотов"})
 
